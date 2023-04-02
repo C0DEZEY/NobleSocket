18caf905c3b67682affda414d666f0cff8cf7a20
Codezey <91379167+C0DEZEY@users.noreply.github.com>
2023-04-02 00:28:57 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!');
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    const parsedMessage = JSON.parse(message);
+
+    // Modify the message object if needed
+
+    const stringifiedMessage = JSON.stringify(parsedMessage);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(stringifiedMessage);
+      }
+    });
+  });
+});
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+server.listen(8080, () => console.log(`Lisening on port :8080`))
